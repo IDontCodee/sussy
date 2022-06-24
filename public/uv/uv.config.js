0b94612996e40e6ec908456bc0dfb56d9a2a5e5b
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-06-24 15:32:33 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/bare/',
+    prefix: '/ultraviolet/',
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
