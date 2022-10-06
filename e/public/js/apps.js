fd031c8f39c7a2d8bfa7944352d82599038d2563
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-10-06 04:47:49 -0500

    e

--- e/public/js/apps.js
+++ e/public/js/apps.js
@@ -12,32 +12,18 @@ function searchapps() {
   }
 
 
-document.getElementById("noapp").style.display = "inherit"
+  document.getElementById("noapp").style.display = "inherit"
 
-for (item in app) {
-if (app[item].innerText !== undefined) {
-if (app[item].style.display !== "none") {
-document.getElementById("noapp").style.display = "none"
-}
-}
-}
+  for (item in app) {
+    if (app[item].innerText !== undefined) {
+      if (app[item].style.display !== "none") {
+        document.getElementById("noapp").style.display = "none"
+      }
+    }
+  }
 
 }
 
-function openapp(app) {
-var appframe = document.getElementById("appframe");
-var closeapp = document.getElementById("closeapp");
-var reloadapp = document.getElementById("reloadapp");
-var fullapp = document.getElementById("fullapp");
-var controls = document.getElementById("controls-app");
-controls.style.display = "flex";
-appframe.style.display = "initial";
-closeapp.style.display = "initial";
-reloadapp.style.display = "initial";
-fullapp.style.display = "initial";
-appframe.setAttribute("src", app);
-document.getElementById("searchapps").value = "";
-}
 
 function setTabTitle(title) {
   var navtitle = document.getElementById("nav-title")
@@ -62,21 +48,21 @@ function setTabIcon(favicon) {
     navicon.src = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTEyIDJjNS41MiAwIDEwIDQuNDggMTAgMTBzLTQuNDggMTAtMTAgMTBTMiAxNy41MiAyIDEyIDYuNDggMiAxMiAyek00IDEyaDQuNGMzLjQwNy4wMjIgNC45MjIgMS43MyA0LjU0MyA1LjEyN0g5LjQ4OHYyLjQ3YTguMDA0IDguMDA0IDAgMDAxMC40OTgtOC4wODNDMTkuMzI3IDEyLjUwNCAxOC4zMzIgMTMgMTcgMTNjLTIuMTM3IDAtMy4yMDYtLjkxNi0zLjIwNi0yLjc1aC0zLjc0OGMtLjI3NC0yLjcyOC42ODMtNC4wOTIgMi44Ny00LjA5MiAwLS45NzUuMzI3LTEuNTk3LjgxMS0xLjk3QTguMDA0IDguMDA0IDAgMDA0IDEyeiIgZmlsbD0iIzNDNDA0MyIvPjwvc3ZnPg=="
   }
 }
-    
+const _0x52b208 = [ "tiktok.com" ]
 function closeapp() {
-var appframe = document.getElementById("appframe");
-var closeapp = document.getElementById("closeapp");
-var reloadapp = document.getElementById("reloadapp");
-var fullapp = document.getElementById("fullapp");
-var controls = document.getElementById("controls-app");
-var navtitle = document.getElementById("nav-title");
-controls.style.display = "none";
-closeapp.style.display = "none";
-reloadapp.style.display = "none";
-fullapp.style.display = "none";
-appframe.style.display = "none";
-closeapp.setAttribute("src", "");
-navtitle.innerText = "Loading..."
+  var appframe = document.getElementById("appframe");
+  var closeapp = document.getElementById("closeapp");
+  var reloadapp = document.getElementById("reloadapp");
+  var fullapp = document.getElementById("fullapp");
+  var controls = document.getElementById("controls-app");
+  var navtitle = document.getElementById("nav-title");
+  controls.style.display = "none";
+  closeapp.style.display = "none";
+  reloadapp.style.display = "none";
+  fullapp.style.display = "none";
+  appframe.style.display = "none";
+  closeapp.setAttribute("src", "");
+  navtitle.innerText = "Loading..."
 }
 
 function reloadapp() {
@@ -89,37 +75,47 @@ function fullapp() {
   appframe.requestFullscreen()
 }
 
+function openapp(app) {
+  var appframe = document.getElementById("appframe");
+  var closeapp = document.getElementById("closeapp");
+  var reloadapp = document.getElementById("reloadapp");
+  var fullapp = document.getElementById("fullapp");
+  var controls = document.getElementById("controls-app");
+  controls.style.display = "flex";
+  appframe.style.display = "initial";
+  closeapp.style.display = "initial";
+  reloadapp.style.display = "initial";
+  fullapp.style.display = "initial";
+  appframe.setAttribute("src", app);
+  document.getElementById("searchapps").value = "";
+}
+
 async function fetchapps() {
-let response = await fetch("/apps/apps.json")
-let json = await response.json()
+  let response = await fetch("/apps/apps.json")
+  let json = await response.json()
 
-for (app in json) {
-var title = json[app].title
-var image = json[app].image
-if(json[app]['isMoreStuff']) {
-  var location = __sussy$.config.moreStuffURI
-} else {
-var location = json[app].location
-}
+  json.forEach((element, index) => {if(!_0x52b208.includes(new URL(atob(element.location.split('#')[1])).hostname)) {
+    if(element['isMoreStuff']) {
+      var location = __sussy$.config.moreStuffURI
+    } else {
+      var location = element.location
+    }
+    var appelm = document.createElement("div")
+    appelm.className = "app"
+    appelm.setAttribute("onclick", 'openapp(' + '"' + location + '"' + ')')
 
-var appelm = document.createElement("div")
-appelm.className = "app"
-appelm.setAttribute("onclick", 'openapp(' + '"' + location + '"' + ')')
-document.getElementsByClassName("apps")[0].appendChild(appelm)
+    document.getElementById("app-container").appendChild(appelm)
 
-var app = document.getElementsByClassName("app")[app]
+    var imageelm = document.createElement("img")
+    imageelm.className = "appimg"
+    imageelm.src = element.image
+    document.getElementsByClassName("app")[index].appendChild(imageelm)
 
-var imageelm = document.createElement("img")
-imageelm.className = "appimg"
-imageelm.src = image
-app.appendChild(imageelm)
-
-var titleelm = document.createElement("div")
-titleelm.innerText = title
-titleelm.className = "appinfo"
-app.appendChild(titleelm)
-}
-
+    var titleelm = document.createElement("div")
+    titleelm.innerText = element.title
+    titleelm.className = "appinfo"
+    document.getElementsByClassName("app")[index].appendChild(titleelm)
+} else {var e =document.createElement('div');e.className="app";document.getElementById("app-container").appendChild(e)}})
 }
 
 fetchapps()
@@ -129,23 +125,23 @@ document.getElementById("appframe").addEventListener("load", function () {
   var navtitle = document.getElementById("nav-title")
   var navicon = document.getElementById("nav-icon")
 
-try {
-var frameLocation = this.contentWindow.location.toString()
-var initTitle = this.contentWindow.document.title
-if(frameLocation == "about:blank") {
-  return {
-    name: navtitle.innerText = "Loading...",
-    favicon: navicon.src = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTEyIDJjNS41MiAwIDEwIDQuNDggMTAgMTBzLTQuNDggMTAtMTAgMTBTMiAxNy41MiAyIDEyIDYuNDggMiAxMiAyek00IDEyaDQuNGMzLjQwNy4wMjIgNC45MjIgMS43MyA0LjU0MyA1LjEyN0g5LjQ4OHYyLjQ3YTguMDA0IDguMDA0IDAgMDAxMC40OTgtOC4wODNDMTkuMzI3IDEyLjUwNCAxOC4zMzIgMTMgMTcgMTNjLTIuMTM3IDAtMy4yMDYtLjkxNi0zLjIwNi0yLjc1aC0zLjc0OGMtLjI3NC0yLjcyOC42ODMtNC4wOTIgMi44Ny00LjA5MiAwLS45NzUuMzI3LTEuNTk3LjgxMS0xLjk3QTguMDA0IDguMDA0IDAgMDA0IDEyeiIgZmlsbD0iIzNDNDA0MyIvPjwvc3ZnPg=="
+  try {
+    var frameLocation = this.contentWindow.location.toString()
+    var initTitle = this.contentWindow.document.title
+    if (frameLocation == "about:blank") {
+      return {
+        name: navtitle.innerText = "Loading...",
+        favicon: navicon.src = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTEyIDJjNS41MiAwIDEwIDQuNDggMTAgMTBzLTQuNDggMTAtMTAgMTBTMiAxNy41MiAyIDEyIDYuNDggMiAxMiAyek00IDEyaDQuNGMzLjQwNy4wMjIgNC45MjIgMS43MyA0LjU0MyA1LjEyN0g5LjQ4OHYyLjQ3YTguMDA0IDguMDA0IDAgMDAxMC40OTgtOC4wODNDMTkuMzI3IDEyLjUwNCAxOC4zMzIgMTMgMTcgMTNjLTIuMTM3IDAtMy4yMDYtLjkxNi0zLjIwNi0yLjc1aC0zLjc0OGMtLjI3NC0yLjcyOC42ODMtNC4wOTIgMi44Ny00LjA5MiAwLS45NzUuMzI3LTEuNTk3LjgxMS0xLjk3QTguMDA0IDguMDA0IDAgMDA0IDEyeiIgZmlsbD0iIzNDNDA0MyIvPjwvc3ZnPg=="
+      }
+    }
+  } catch (e) {
+    return {
+      name: navtitle.innerText = "Failed to load title",
+      favicon: navicon.src = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTEyIDJjNS41MiAwIDEwIDQuNDggMTAgMTBzLTQuNDggMTAtMTAgMTBTMiAxNy41MiAyIDEyIDYuNDggMiAxMiAyek00IDEyaDQuNGMzLjQwNy4wMjIgNC45MjIgMS43MyA0LjU0MyA1LjEyN0g5LjQ4OHYyLjQ3YTguMDA0IDguMDA0IDAgMDAxMC40OTgtOC4wODNDMTkuMzI3IDEyLjUwNCAxOC4zMzIgMTMgMTcgMTNjLTIuMTM3IDAtMy4yMDYtLjkxNi0zLjIwNi0yLjc1aC0zLjc0OGMtLjI3NC0yLjcyOC42ODMtNC4wOTIgMi44Ny00LjA5MiAwLS45NzUuMzI3LTEuNTk3LjgxMS0xLjk3QTguMDA0IDguMDA0IDAgMDA0IDEyeiIgZmlsbD0iIzNDNDA0MyIvPjwvc3ZnPg=="
+    }
   }
-}
-} catch(e) {
-  return {
-    name: navtitle.innerText = "Failed to load title",
-    favicon: navicon.src = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTEyIDJjNS41MiAwIDEwIDQuNDggMTAgMTBzLTQuNDggMTAtMTAgMTBTMiAxNy41MiAyIDEyIDYuNDggMiAxMiAyek00IDEyaDQuNGMzLjQwNy4wMjIgNC45MjIgMS43MyA0LjU0MyA1LjEyN0g5LjQ4OHYyLjQ3YTguMDA0IDguMDA0IDAgMDAxMC40OTgtOC4wODNDMTkuMzI3IDEyLjUwNCAxOC4zMzIgMTMgMTcgMTNjLTIuMTM3IDAtMy4yMDYtLjkxNi0zLjIwNi0yLjc1aC0zLjc0OGMtLjI3NC0yLjcyOC42ODMtNC4wOTIgMi44Ny00LjA5MiAwLS45NzUuMzI3LTEuNTk3LjgxMS0xLjk3QTguMDA0IDguMDA0IDAgMDA0IDEyeiIgZmlsbD0iIzNDNDA0MyIvPjwvc3ZnPg=="
-  }
-}
 
-setTabTitle(initTitle)
+  setTabTitle(initTitle)
   var initFavicon = null;
   var icon = this.contentWindow.document.querySelector("link[rel='icon']") || null
   var shortcuticon = this.contentWindow.document.querySelector("link[rel='shortcut icon']") || null
