28cd126caa55eb716027aa0246b64ef261fc184f
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-12-12 16:55:32 -0600

    revert

--- README.md
+++
@@ -1,3 +0,0 @@
-Nothing here
-
-[![Join us on Discord](https://invidget.switchblade.xyz/J3VPy5Vy8x?theme=light)](https://discord.gg/J3VPy5Vy8x)
--- e/public/sw-handler.js
+++
@@ -1,9 +0,0 @@
-importScripts('/js/sw-scripts/uv.sw.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', event =>
-    event.respondWith(
-        sw.fetch(event)
-    )
-);
